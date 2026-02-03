c36be1f943708edb908398173954c31e6ccf5bc2
DonovanJ204 <donovan.jeannot.stcharles@gmail.com>
2026-02-03 09:58:24 +0000

    Fin du tp

---
+++ script.js
@@ -0,0 +1,49 @@
+console.log("Script chargé !");
+
+const bt = document.querySelector("#bt");
+
+bt.addEventListener("click", (event) => {
+  document.getElementById("details").hidden = true;
+  document.getElementById("details1").hidden = false;
+});
+
+const bt1 = document.querySelector("#bt1");
+
+bt1.addEventListener("click", (event) => {
+  document.getElementById("details1").hidden = true;
+  document.getElementById("details").hidden = false;
+});
+
+const titre = document.querySelector('#titre-principal');
+
+titre.addEventListener('click', () => {
+  titre.textContent = "Vous avez cliqué sur le titre !";
+  console.log('Bouton cliqué !');
+});
+
+const themeButton = document.querySelector('#theme-toggle');
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        themeButton.textContent = "Mode Clair";
+    } else {
+        themeButton.textContent = "Mode Sombre";
+    }
+
+});
+
+const tousLesTitresa = document.querySelectorAll('a');
+console.log(tousLesTitresa);
+
+const href = lien.getAttribute('href');
+const section = document.querySelector(href);
+
+tousLesTitresa.forEach(titre => {
+    titre.addEventListener('click', () => {
+        event.preventDefault()
+        console.log('Bouton cliqué !');
+        section.scrollIntoView( {behavior: "smooth"} );
+    });
+});
